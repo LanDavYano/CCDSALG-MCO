87adfc659db6f0dd7a61e2c90caa2b8cadc17a06
LanDavYano <lance_jardiniano@dlsu.edu.ph>
2025-07-04 22:18:43 +0800

    added lennard's evaluation

--- MCO1/evaluation.c
+++ MCO1/evaluation.c
@@ -25,7 +25,7 @@ int stringToInt(char *multipleDigit)
     return atoi(multipleDigit);
 }
 
-int performArithmetic(int firstNumber, int secondNumber, char op) 
+int performOperation(int firstNumber, int secondNumber, char op) 
 {
     switch (op) 
     {
@@ -65,27 +65,50 @@ int performArithmetic(int firstNumber, int secondNumber, char op)
     return -1;
 }
 
-int performBoolean(int firstNumber, int secondNumber, char op) 
-{
-    switch (op) 
-    {
-        case '>':
-            return firstNumber > secondNumber;
-        case '<':
-            return firstNumber < secondNumber;
-        case '>=':
-            return firstNumber >= secondNumber;
-        case '<=':
-            return firstNumber <= secondNumber;
-        case '==':
-            return firstNumber == secondNumber;
-        case '!=':
-            return firstNumber != secondNumber;
-        case '&&':
-            return firstNumber && secondNumber;
-        case '||':
-            return firstNumber || secondNumber;
-        default:
-            return -1; 
+int evaluation(Queue *queue, str256 token){
+
+    //local stack OPERAND 
+    StackOperand stackOperand;
+    createStackOperator(stackOperand);
+
+    //dequeing and putting into STACK
+    if(dequeue(queue, token)){
+
+        for(int i=0;i<token.length;i++){
+
+            int flagFound = false;
+
+            //check if it is an operator
+            while(operators && flagFound == false){
+                if(operators[i] == token[i]){
+                    flagFound = true;
+                }
+                int i++;
+            }
+
+            if(flagFound == true){
+
+                //pop pop from the stack
+                //then evaluate if numbers in stack is 2 or more
+                //if it is then evaluate the expression then push to stack
+                bool first_pop = popOperand(stackOperand, token[i]);
+                bool second_pop = popOperand(stackOperand, token[i+1]);
+
+                if(first_pop && second_pop){
+
+                    int left_number = stackOperand[i];
+                    int right_number = stackOperand[i+1];
+                    char current_op = token[i];
+                    char curr_result[20];  // Make sure it's large enough
+                    sprintf(curr_result, "%d%c%d", left_number, current_op, right_number);
+                    pushOperand(stackOperand,curr_result);
+                }
+
+            }else{
+                //push to stack
+                int push_num = charToInt(token[i]);
+                pushOperand(stackOperand, push_num);
+            }
+        }
     }
 }
--- MCO1/evaluation.h
+++ MCO1/evaluation.h
@@ -4,11 +4,14 @@
 #include <ctype.h>
 #include <stdlib.h>
 #include <math.h>
+#include "queue.h"
+#include "stack.h"
+#include "conversion.h"
 
 int charToInt(char singleDigit);
 char intToChar(int singleDigit);
 int stringToInt(char *multipleDigit);
 int performOperation(int firstNumber, int secondNumber, char op);
-
+int evaluation(Queue *queue, str256 token, StackOperand *stack );
 
 #endif
